ee083e51d7cffe99a279d2fd5ddaf689c719aaa1
kapold <antonadamovich@mail.ru>
2022-10-15 10:10:56 +0300

    Add maven-labs

---
+++ maven-labs/src/main/java/com/epam/training/student_anton_adamovich/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_anton_adamovich;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven-labs/src/test/java/com/epam/training/student_anton_adamovich/AppTest.java
@@ -0,0 +1,20 @@
+package com.epam.training.student_anton_adamovich;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
